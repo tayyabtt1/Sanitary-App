84bfeaff08f95d3aef02183aef3760237451b796
tayyabtt1 <f2024376199@umt.edu.pk>
2026-07-26 13:29:10 +0500

    feat: implement bottom navigation shell with 3-tab routing

---
+++ lahore_sanitery/lib/widgets/main_nav.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../screens/home_screen.dart';
+import '../screens/products_screen.dart';
+import '../screens/manage_screen.dart';
+
+class MainNav extends StatefulWidget {
+  const MainNav({super.key});
+
+  @override
+  State<MainNav> createState() => _MainNavState();
+}
+ 
+class _MainNavState extends State<MainNav> {
+  int _currentIndex = 0;
+
+  final _screens = const [
+    HomeScreen(),
+    ProductsScreen(),
+    ManageScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.inventory_2), label: 'Products'),
+          BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'Manage'),
+        ],
+      ),
+    );
+  }
+}
